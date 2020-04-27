35fb98d606e01c159b41562cc4170cb52bfadce8
yardenSchwartz <49039527+yardenSchwartz@users.noreply.github.com>
2020-04-27 13:30:51 +0300

    Update menuCode.js

--- jvCode/menuCode.js
+++ jvCode/menuCode.js
@@ -1,6 +1,35 @@
 
+//initialize array for saving users
+var allUsers;
+
+$(document).ready(function(){
+    
+    var allUsers = new Array();
+    var defaultUser = {
+        name:"p",
+        password:"p"
+    };
+    allUsers.push(defaultUser);
+
+    if(localStorage.getItem("allUsers")==null){
+        localStorage.setItem("allUsers",JSON.stringify(allUsers));
+    }
 
 
+    //check if the default user exist in the array
+    if(containUser(defaultUser)==false){
+      allUsers = JSON.parse(localStorage.getItem("allUsers"));
+      allUsers.push(defaultUser);
+      localStorage.setItem("allUsers", JSON.stringify(allUsers));
+    }
+
+    if(localStorage.getItem("allUsers"==null)){
+        localStorage.setItem("allUsers",JSON.stringify(allUsers));
+    }
+})
+
+
+/*************switching window**************/
 function changeWindow(id) {
     let section = document.getElementsByTagName('section');
 
@@ -22,3 +51,5 @@ function changeWindowById(id) {
     document.getElementById(id).style.display = "block"
 }
 
+
+  
