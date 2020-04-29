d29afdaa1ef75f0d294c04fbfd9a1c3c2b0c53a0
eden <eden04060925@gmail.com>
2020-04-29 23:15:04 +0300

    BHJJK

--- jvCode/app.js
+++ jvCode/app.js
@@ -1,3 +1,4 @@
+
 var context;
 var shape = new Object();
 var board;
@@ -6,6 +7,14 @@ var pac_color;
 var start_time;
 var time_elapsed;
 var interval;
+var leftCode;
+var rightCode;
+var upCode;
+var downCode;
+
+var firstAngle=0.15;
+var secondAngle=1.95;
+
 
 
 
@@ -47,7 +56,7 @@ function Start() {
 		
  board = [
 	[4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
-	[4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+	[4, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
 	[4, 0, 4, 4, 0, 4, 0, 4, 4, 0, 4, 4, 4, 0, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 0, 4],
 	[4, 0, 4, 4, 0, 4, 0, 4, 0, 0, 0, 4, 4, 0, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0, 4, 0, 0, 0, 0, 4],
 	[4, 0, 4, 0, 0, 4, 0, 4, 0, 0, 0, 4, 4, 0, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0, 4, 0, 4, 4, 0, 4],
@@ -90,7 +99,7 @@ function Start() {
 		//put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
 		for (var j = 0; j < 30; j++) {
 	
-			if(board[i][j]==4){
+			if(board[i][j]==4||board[i][j]==7){
 
 			}
 			else {
@@ -130,17 +139,32 @@ function Start() {
 	addEventListener(
 		"keydown",
 		function(e) {
-			keysDown[e.keyCode] = true;
+			let left=leftChoose;
+			let right=rightChoose;
+			let up=upChoose;
+			let down=downChoose;
+			let code= e.code
+			if(e.code==leftChoose||e.code==rightChoose||e.code==downChoose||e.code==upChoose){
+				keysDown[e.code] = true;
+			}
 		},
 		false
 	);
 	addEventListener(
 		"keyup",
 		function(e) {
-			keysDown[e.keyCode] = false;
+			if(e.code==leftChoose||e.code==rightChoose||e.code==downChoose||e.code==upChoose){
+				keysDown[e.code] = false;
+			}
 		},
 		false
 	);
+
+	window.addEventListener("keydown", function(e) {
+		if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+			e.preventDefault();
+		}
+	}, false);
 	interval = setInterval(UpdatePosition, 250);
 }
 
@@ -155,16 +179,17 @@ function findRandomEmptyCell(board) {
 }
 
 function GetKeyPressed() {
-	if (keysDown[38]) {
+
+	if (keysDown[upChoose]) {
 		return 1;
 	}
-	if (keysDown[40]) {
+	if (keysDown[downChoose]) {
 		return 2;
 	}
-	if (keysDown[37]) {
+	if (keysDown[leftChoose]) {
 		return 3;
 	}
-	if (keysDown[39]) {
+	if (keysDown[rightChoose]) {
 		return 4;
 	}
 }
@@ -183,15 +208,36 @@ function Draw() {
 			let centerX=i *cellWidth+0.5*cellWidth;
 			let centerY=j *cellHeight+0.5*cellHeight;
 			
+			
 			/*PACMAN */
 			if (board[i][j] == 2) {
 				context.beginPath();
-				context.arc(centerX, centerY, 9, 0.15 * Math.PI, 1.95 * Math.PI); // half circle
+				// context.arc(centerY, centerX, 9, 0.15 * Math.PI, 1.95 * Math.PI); // half circle
+				context.arc(centerX, centerY, 12,firstAngle* Math.PI,secondAngle * Math.PI); // half circle
 				context.lineTo(centerX, centerY);
 				context.fillStyle = pac_color; //color
 				context.fill();
+
+				context.arc(centerX, centerY, 12,firstAngle* Math.PI,secondAngle * Math.PI); // half circle
+				context.lineWidth =1;
+				context.strokeStyle = "black";
+				context.stroke();
+
 				context.beginPath();
-				context.arc(centerX+ 1, centerY-6, 1.5, 0, 2 * Math.PI); // circle
+				if(firstAngle==1.6){//up
+					context.arc(centerX-7, centerY-6, 1.5, 0, 2 * Math.PI); // circle
+
+				}
+				else{
+					if(firstAngle==0.7){//down
+						context.arc(centerX+ 4, centerY-2, 1.5, 0, 2 * Math.PI); // circle
+
+					}
+					else{
+						context.arc(centerX+ 1, centerY-6, 1.5, 0, 2 * Math.PI); // circle
+					}
+				}
+
 				context.fillStyle = "black"; //color
 				context.fill();
 				
@@ -199,21 +245,43 @@ function Draw() {
 			/*Balls*/
 			 if (board[i][j] == 5) {
 				context.beginPath();
-				context.arc(centerX, centerY, 7.5, 0, 2 * Math.PI); // circle
+				context.arc(centerX, centerY,5, 0, 2 * Math.PI); // circle
 				context.fillStyle =document.getElementById("colorpadfirst").value;
-				context.fill();
+				context.fill();	
+				
+				context.font= "bold 10px Ariel";
+				context.fillStyle =invertColor(document.getElementById("colorpadfirstthird").value);
+				context.fillText('5',centerX-2,centerY +3);
+				
+
 			 }
 			 if (board[i][j] == 15) {
 				context.beginPath();
-				context.arc(centerX, centerY, 7.5, 0, 2 * Math.PI); // circle
+				context.arc(centerX, centerY, 9, 0, 2 * Math.PI); // circle
 				context.fillStyle =document.getElementById("colorpadsecond").value;
 				context.fill();
+
+
+
+				context.font= "bold 12px Ariel";
+				context.fillStyle =invertColor(document.getElementById("colorpadfirstthird").value);
+				context.fillText('15',centerX-7,centerY +4);
 			 }
 			 if (board[i][j] == 25) {
 				context.beginPath();
-				context.arc(centerX, centerY, 7.5, 0, 2 * Math.PI); // circle
+				context.arc(centerX, centerY,12, 0, 2 * Math.PI); // circle
 				context.fillStyle =document.getElementById("colorpadfirstthird").value;
 				context.fill();
+				
+				let negativeColor=invertColor(document.getElementById("colorpadfirstthird").value);
+				context.arc(centerX, centerY, 12, 0, 2 * Math.PI);
+				context.lineWidth = 2;
+				context.strokeStyle = "red";
+				context.stroke();
+
+				context.font= "bold 15px Lucida Console";
+				context.fillStyle =negativeColor
+				context.fillText('25',centerX-10,centerY +5);
 				
 			 }
 				/*Walls*/
@@ -221,52 +289,100 @@ function Draw() {
 			 if (board[i][j] == 4) {
 				context.beginPath();
 				context.rect(start.x, start.y,cellWidth,cellHeight);
-				context.fillStyle = "blue"; //color
+				context.fillStyle = "#8FBC8F"; //color
 				context.fill();
+				context.lineWidth = 1;
+				context.strokeStyle = "#696969";
+				context.strokeRect(start.x, start.y,cellWidth,cellHeight);
+				
+				
 			}
+			if(i==1&&j==1){
+			if (board[i][j] == 7) {
+				context.beginPath();
+				// context.rect(start.x, start.y,cellWidth,cellHeight);
+				
+				img=new Image();
+				img.src="images/images-removebg-preview.png";
+				context.beginPath();
+				context.drawImage(img, start.x, start.y,cellWidth*1.2,cellHeight*1.2);
+				
+			
+				
+				
+			}
+		}
 	
 		}
 	}
+
+	
+
+		
+
 }
 
 function UpdatePosition() {
+	let lasti=shape.i;
+	let lastj=shape.j;
 	board[shape.i][shape.j] = 0;
 	var x = GetKeyPressed();
-	if (x == 1) {
+	if (x == 1) {//up
 		if (shape.j > 0 && board[shape.i][shape.j - 1] != 4) {
 			shape.j--;
+			firstAngle=1.6;
+			secondAngle=3.35;
 		}
 	}
-	if (x == 2) {
-		if (shape.j < 9 && board[shape.i][shape.j + 1] != 4) {
+	if (x == 2) {//down
+		if (shape.j < 29 && board[shape.i][shape.j + 1] != 4) {
 			shape.j++;
+			firstAngle=0.7;
+			secondAngle=2.3;
 		}
 	}
-	if (x == 3) {
+	if (x == 3) {//left
 		if (shape.i > 0 && board[shape.i - 1][shape.j] != 4) {
 			shape.i--;
+			firstAngle=1.1;
+			secondAngle=2.9;
 		}
 	}
-	if (x == 4) {
-		if (shape.i < 9 && board[shape.i + 1][shape.j] != 4) {
+	if (x == 4) {//right
+		if (shape.i < 29 && board[shape.i + 1][shape.j] != 4) {
 			shape.i++;
+			firstAngle=0.15;
+			secondAngle=1.85;
 		}
 	}
-	if (board[shape.i][shape.j] == 1) {
-		score++;
+	let newi=shape.i;
+	let newj=shape.j;
+	
+	//lblScore
+	
+	if (board[shape.i][shape.j] == 5) {
+		score=score+5;
 	}
+	if (board[shape.i][shape.j] == 15) {
+		score=score+15;
+	}
+	if (board[shape.i][shape.j] == 25) {
+		score=score+25;
+	}
+	document.getElementById("lblScore").value=score;
 	board[shape.i][shape.j] = 2;
 	var currentTime = new Date();
 	time_elapsed = (currentTime - start_time) / 1000;
-	if (score >= 20 && time_elapsed <= 10) {
-		pac_color = "green";
-	}
-	if (score == 50) {
-		window.clearInterval(interval);
-		window.alert("Game completed");
-	} else {
+	// if (score >= 20 && time_elapsed <= 10) {
+	// 	pac_color = "green";
+	// }
+	// if (score == 50) {
+	// 	window.clearInterval(interval);
+	// 	window.alert("Game completed");
+	// } else {
 		Draw();
-	}
+
+	// }
 }
 
 function startNewGame(){
@@ -274,3 +390,47 @@ function startNewGame(){
 	//init timer
 	start();
 }
+
+// function invertColor(hex) {
+//     if (hex.indexOf('#') === 0) {
+//         hex = hex.slice(1);
+//     }
+//     // convert 3-digit hex to 6-digits.
+//     if (hex.length === 3) {
+//         hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+//     }
+//     if (hex.length !== 6) {
+//         throw new Error('Invalid HEX color.');
+//     }
+//     // invert color components
+//     var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+//         g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+//         b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+//     // pad each with zeros and return
+//     return '#' + padZero(r) + padZero(g) + padZero(b);
+// }
+
+// function padZero(str, len) {
+//     len = len || 2;
+//     var zeros = new Array(len).join('0');
+//     return (zeros + str).slice(-len);
+// }
+
+function invertColor(hexTripletColor) {
+	var color = hexTripletColor;
+	color = color.substring(1); // remove #
+	color = parseInt(color, 16); // convert to integer
+	color = 0xFFFFFF ^ color; // invert three bytes
+	color = color.toString(16); // convert to hex
+	color = ("000000" + color).slice(-6); // pad with leading zeros
+	color = "#" + color; // prepend #
+	return color;
+}
+$(".demo").each(function() {
+	var c1 = randomColor();
+	var c2 = invertColor(c1);
+	$(this).text(c1 + " " + c2).css({
+		"color": c1,
+		"background-color": c2
+	});
+});
